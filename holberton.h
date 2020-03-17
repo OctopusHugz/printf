e021471cdf1c33f574e3ab554c5246bbc1243b3d
GunterPearson <1715@holbertonschool.com>
2020-03-17 18:17:17 +0000

    updated to line up with master

---
+++ holberton.h
@@ -0,0 +1,71 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+
+int _strlen(const char *format);
+
+int count_args(char *holder);
+
+/**
+ * struct specifier - struct type
+ *
+ * @string: The string
+ * @vpoin: The function associated
+ */
+
+typedef struct specifier
+{
+	char *string;
+	int (*vpoin)();
+
+} specifier;
+
+/**
+ * struct hex - converts hex
+ * @num: int equal
+ * @h: hex conversion
+ *
+ */
+
+typedef struct hex
+{
+	unsigned int num;
+	char h;
+} hex;
+
+int print_hex_lower(va_list args);
+
+int hex_recursion_lower(unsigned int n);
+
+int print_number(int n);
+
+int print_pointer(va_list args);
+
+int print_char(va_list args);
+
+int print_int(va_list args);
+
+int print_float(va_list args);
+
+int print_string(va_list args);
+
+int print_dec(va_list args);
+
+int print_mod(void);
+
+int modulo(va_list args, char c);
+
+int print_binary(va_list args);
+
+int binary_recursion(unsigned int n);
+
+int print_number_ui(unsigned int n);
+
+#endif /* HOLBERTON_H */
---
+++ print_nums.c
@@ -0,0 +1,87 @@
+#include <limits.h>
+#include "holberton.h"
+
+/**
+ * print_int - prints an integer
+ * @args: integer to print from va_list
+ * Return: int
+ */
+
+int print_int(va_list args)
+{
+	int i = 0;
+	int holder;
+
+	holder = va_arg(args, int);
+	i += print_number(holder);
+	return (i - 2);
+}
+
+/**
+ * print_number - start of funciton
+ * @n: number given to print
+ * Return: int
+ */
+
+int print_number(int n)
+{
+	unsigned int ui = (unsigned int)INT_MAX + 1;
+	int i = 0;
+
+	if (n == INT_MIN)
+	{
+		_putchar('-');
+		i++;
+		i += print_number_ui(ui);
+		return (i);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		i++;
+	}
+	if (n / 10)
+	{
+		i += print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+	i++;
+	return (i);
+}
+
+
+/**
+ * print_dec - prints a decimal value
+ * @args: decimal value to print from va_list
+ * Return: int
+ */
+
+int print_dec(va_list args)
+{
+	int i = 0;
+	int holder;
+
+	holder = va_arg(args, int);
+	i += print_number(holder);
+	return (i - 2);
+}
+
+
+/**
+ * print_number_ui - prints an unsigned integer
+ * @n: unsigned integer to print
+ * Return: number of integers printed
+ */
+
+int print_number_ui(unsigned int n)
+{
+	int i = 0;
+
+	if (n / 10)
+		i += print_number_ui(n / 10);
+	_putchar(n % 10 + '0');
+	i++;
+	return (i);
+}
